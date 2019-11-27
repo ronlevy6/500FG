7f975955c949bffc8f569d96af91957e839b946a
ronlevy <ronlevy6@gmail.com>
2019-11-27 11:17:19 +0200

    adding option to run calc_dist_in_df on the cluster when passing executor as a parameter

--- data_manipulation.py
+++ data_manipulation.py
@@ -2,7 +2,6 @@ import numpy as np
 from tqdm import tqdm
 import pickle
 
-
 def calc_row_col_dist(arr_2d, node_idx, k=50, with_distance=False,axis=0):
     if isinstance(arr_2d, str):
         with open(arr_2d, 'rb') as f:
@@ -16,7 +15,7 @@ def calc_row_col_dist(arr_2d, node_idx, k=50, with_distance=False,axis=0):
         return [x[0] for x in idx_dist_lst[:k]]
 
 
-def calc_dist_in_df(df, by_idx=True, k=50, with_distance=False):
+def calc_dist_in_df(df, by_idx=True, k=50, with_distance=False, executor=None):
     arr_2d = df.to_numpy()
     dist_lsts = dict()
     if by_idx:
@@ -26,7 +25,10 @@ def calc_dist_in_df(df, by_idx=True, k=50, with_distance=False):
         vals_to_check = df.columns
         axis=0
     for idx, val in tqdm(enumerate(vals_to_check)):
-        nearest_points = calc_row_col_dist(arr_2d, idx, axis=axis, k=k, with_distance=with_distance)
+        if executor is not None:
+            nearest_points = executor.submit(calc_row_col_dist,arr_2d, idx, axis=axis, k=k, with_distance=with_distance)
+        else:
+            nearest_points = calc_row_col_dist(arr_2d, idx, axis=axis, k=k, with_distance=with_distance)
         dist_lsts[val] = nearest_points
     return dist_lsts
 
