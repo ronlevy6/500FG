3bd2990881c627b319efa5f93db92367e6868d35
ronlevy <ronlevy6@gmail.com>
2019-12-04 17:39:26 +0200

    get angle of points as graph of circle

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import uuid
 import os
+import math
 
 
 def create_tmp_file(dir, suffix=None):
@@ -7,3 +8,13 @@ def create_tmp_file(dir, suffix=None):
     if suffix is not None:
         filename += '.{}'.format(suffix)
     return os.path.join(dir, filename)
+
+
+# center - x0,y0
+def get_angle_in_circle(x, y, x0, y0):
+    (dx, dy) = (x0-x, y-y0)
+    # angle = atan(float(dy)/float(dx))
+    angle = math.degrees(math.atan2(float(dy), float(dx)))
+#     if angle < 0:
+#         angle += 180
+    return angle
