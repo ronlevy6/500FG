1db5fcbb5b5d092b9ef8977221a47d3ce99913e6
ronlevy <ronlevy6@gmail.com>
2020-10-08 17:22:33 +0300

    bug fix in regression module since we check phenotype~tissue relation and not tissue1+phenotype~tissue2

--- regression.py
+++ regression.py
@@ -31,7 +31,7 @@ def fit_tissues(x, y, fit_intercept=True):
     return model_sm
 
 
-def make_df(dict_to_use, make_symmetric):
+def make_df(dict_to_use, make_symmetric, from_dict_orient='columns'):
     dict_of_dfs = dict()
     for k_tup in dict_to_use:
         tmp = dict_to_use[k_tup]
@@ -41,9 +41,10 @@ def make_df(dict_to_use, make_symmetric):
                 new_tmp.setdefault(k1, dict())[k2] = v
                 if make_symmetric and (k2 not in tmp or k1 not in tmp[k2]):
                     new_tmp.setdefault(k2, dict())[k1] = v
-        tmp_df = pd.DataFrame.from_dict(new_tmp, orient='index')
-        tmp_df = tmp_df[sorted(tmp_df.columns)]
-        tmp_df = tmp_df.reindex(tmp_df.columns)
+        tmp_df = pd.DataFrame.from_dict(new_tmp, orient=from_dict_orient)
+        if make_symmetric:
+            tmp_df = tmp_df[sorted(tmp_df.columns)]
+            tmp_df = tmp_df.reindex(tmp_df.columns)
         dict_of_dfs[k_tup] = tmp_df
     return dict_of_dfs
 
@@ -59,10 +60,10 @@ def filter_reg_dict(reg_dict, use_attr=False):
 
 def filter_reg_dict_cluster(outer_key, inner_reg_futs_dict, use_attr=False):
     filtered_reg_dict = dict()
-    for (t1, t2, attr_col), reg_sm in inner_reg_futs_dict.items():
+    for (t1, attr_col), reg_sm in inner_reg_futs_dict.items():
         if use_attr:
             gender, age_group, death_group, states_df_type, to_filter, idx = outer_key
-            new_k = gender, age_group, death_group, states_df_type, to_filter, t1, t2, idx, attr_col
+            new_k = gender, age_group, death_group, states_df_type, to_filter, t1, idx, attr_col
         else:
             new_k = outer_key
         if reg_sm is not None:
@@ -72,15 +73,15 @@ def filter_reg_dict_cluster(outer_key, inner_reg_futs_dict, use_attr=False):
 
 
 def organize_reg_dict(filtered_reg_dict, separate_s1_s2=True, create_dfs=True, make_symmetric=True,
-                      use_attr=False, to_merge=True, assert_key=True):
+                      use_attr=False, to_merge=True, assert_key=True, from_dict_orient='columns'):
     if isinstance(filtered_reg_dict, list) and to_merge:
         flatten_reg_dict = flatten_dict(filtered_reg_dict, assert_disjoint=assert_key)
     else:
         flatten_reg_dict = filtered_reg_dict
     if use_attr:
-        key_tup_to_use = 4
-    else:
         key_tup_to_use = 3
+    else:
+        key_tup_to_use = 2
 
     if separate_s1_s2:
         filtered_reg_dict_s1 = dict()
@@ -88,29 +89,29 @@ def organize_reg_dict(filtered_reg_dict, separate_s1_s2=True, create_dfs=True, m
         for k, v in tqdm(flatten_reg_dict.items()):
             keys = k[:-key_tup_to_use]
             if use_attr:
-                t1, t2, idx, attr = k[-key_tup_to_use:]
+                tissue, idx, attr = k[-key_tup_to_use:]
             else:
-                t1, t2, idx = k[-key_tup_to_use:]
+                tissue, idx = k[-key_tup_to_use:]
             params, variables_pvalues, f_pvalue = v
-            if t1 not in params and t2 not in params:
+            if attr not in params:
                 continue
-            try:
-                curr_val = params[t1]
-            except KeyError:
-                curr_val = params[t2]
+            curr_val = params[attr]
             if idx == 0:
                 curr_d = filtered_reg_dict_s1
-            else:
+            elif idx == 1:
                 curr_d = filtered_reg_dict_s2
+            else:
+                raise ValueError("Invalid state index")
 
             if use_attr:
-                curr_d.setdefault(keys, dict()).setdefault(t1, dict()).setdefault(t2, dict())[attr] = curr_val
+                curr_d.setdefault(keys, dict()).setdefault(tissue, dict())[attr] = curr_val
             else:
-                curr_d.setdefault(keys, dict()).setdefault(t1, dict())[t2] = curr_val
+                curr_d.setdefault(keys, dict())[tissue] = curr_val
 
         if create_dfs:
-            filtered_reg_dict_s1_dfs = make_df(filtered_reg_dict_s1, make_symmetric)
-            filtered_reg_dict_s2_dfs = make_df(filtered_reg_dict_s2, make_symmetric)
+            filtered_reg_dict_s1_dfs = make_df(filtered_reg_dict_s1, make_symmetric, from_dict_orient=from_dict_orient)
+            filtered_reg_dict_s2_dfs = make_df(filtered_reg_dict_s2, make_symmetric, from_dict_orient=from_dict_orient)
+
             return filtered_reg_dict_s1_dfs, filtered_reg_dict_s2_dfs, flatten_reg_dict
         else:
             return filtered_reg_dict_s1, filtered_reg_dict_s2, flatten_reg_dict
